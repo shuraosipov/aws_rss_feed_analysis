fb5cbb43605948e9baf0feaaadc71a0a1d61d338
shuraosipov <shuraosipov@gmail.com>
2022-10-02 19:14:59 -0400

    added filtering capabilities

--- collector/lambda/lambda_function.py
+++ collector/lambda/lambda_function.py
@@ -20,15 +20,7 @@ def check_if_feed_was_updated_recently(start_time, update_time):
         print(f"No new entries since {date_to_string(start_time)}")
         sys.exit(123)
 
-def convert_feed_to_dataframe(feed, start_time) -> pd.DataFrame:
-    df = feedparser_to_dataframe(feed)
-    df = filter_old_entries(df, start_time)
-    return df
-
-def filter_old_entries(df, start_time) -> pd.DataFrame:
-    return df[df['published'].apply(string_to_date) > start_time]
-
-def feedparser_to_dataframe(feed) -> pd.DataFrame:
+def convert_feedparser_to_dataframe(feed, start_time) -> pd.DataFrame:
     df = pd.DataFrame()
     df['id'] = [entry.id for entry in feed.entries]
     df['category'] = [entry['category'] for entry in feed['entries']]
@@ -36,13 +28,16 @@ def feedparser_to_dataframe(feed) -> pd.DataFrame:
     df['link'] = [entry['link'] for entry in feed['entries']]
     df['published'] = [entry['published'] for entry in feed['entries']]
     df['summary'] = [entry['summary'] for entry in feed['entries']]
+
+    # filter out old entries
+    df = df[df['published'].apply(string_to_date) > start_time]
     return df
 
 def generate_table(feed, days_range) -> pd.DataFrame:   
     start_time = generate_start_date(days_range)
     
     check_new_entries_exist(feed, start_time)
-    return convert_feed_to_dataframe(feed, start_time)
+    return convert_feedparser_to_dataframe(feed, start_time)
 
 
 def lambda_handler(event, context):
@@ -54,7 +49,9 @@ def lambda_handler(event, context):
 
     try:
         feed = feedparser.parse(FEED_URL)
+        
         df = generate_table(feed, DAYS_RANGE)
+        
         s3_uri = upload_to_s3(BUCKET_NAME, df)
         
         publish_sns_message(
